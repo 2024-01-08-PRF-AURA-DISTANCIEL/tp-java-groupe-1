756d72da2abdd43490d640a7e1a6c092bb882b4d
Arthur Molinero <arthur.molinero02@gmail.com>
2024-05-22 14:53:50 +0200

    reparation dossier src

--- src/model/Specie.java
+++ src/model/Specie.java
@@ -1,3 +1,11 @@
 package model;
 
-public class Specie {}
+public abstract class Specie extends Pokemon {
+	public Specie(String speciesName, String type, double lifeInit, double atkInit) {
+		this.speciesName = speciesName;
+		this.type = type;
+		this.lifeInit = lifeInit;
+		this.atkInit = atkInit;
+		}
+
+}
