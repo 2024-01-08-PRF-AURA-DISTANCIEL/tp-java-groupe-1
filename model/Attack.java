4ee5f3ad542a7492d3417325e435a739cf86be09
alex38070 <alexdu714@hotmail.fr>
2024-05-22 11:59:15 +0200

    creation class attaque

--- model/Attack.java
+++ model/Attack.java
@@ -1,5 +1,16 @@
 package model;
 
 public class Attack {
+// cretion class Attack
+    private String name;
+    private String attackType;
+    private int damagePoints;
+
+    // Constructeur du attack
+    public Attack(String name, String attackType, int damagePoints) {
+        this.name = name;
+        this.attackType = attackType;
+        this.damagePoints = damagePoints;
+    }
 
 }
