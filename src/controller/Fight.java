c19d0c22f64c5330b5a867d1f27e0fb187925a4b
alex38070 <alexdu714@hotmail.fr>
2024-05-24 09:31:53 +0200

    first commit

--- src/controller/Fight.java
+++ src/controller/Fight.java
@@ -1,13 +1,15 @@
-package model;
+package controller;
 
-public class Attack {
+public class Fight {
+
+
 // cretion class Attack
     private String name;
     private String attackType;
     private int damagePoints;
 
     // Constructeur du attack
-    public Attack(String name, String attackType, int damagePoints) {
+    public Fight(String name, String attackType, int damagePoints) {
         this.name = name;
         this.attackType = attackType;
         this.damagePoints = damagePoints;
